b8bbf4fa8d722cfb966728c54ada577e2dd4a24c
Mnv2017 <mnv2017@gmx.de>
2021-11-25 18:50:08 +0100

    Lesson 24.11.21 added SelectDate in event

--- src/test/java/com/telran/scheduler/fw/EventHelper.java
+++ src/test/java/com/telran/scheduler/fw/EventHelper.java
@@ -2,7 +2,10 @@ package com.telran.scheduler.fw;
 
 import com.telran.scheduler.model.Event;
 import io.appium.java_client.AppiumDriver;
+import io.appium.java_client.TouchAction;
+import io.appium.java_client.touch.offset.PointOption;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.WebElement;
 
 import java.util.List;
@@ -61,10 +64,116 @@ public class EventHelper extends HelperBase {
         List<WebElement> id = driver.findElements(By.id("row_container_main"));
         int idCount = id.size();
         System.out.println("*** Total events quantity: " + idCount);
-
         return idCount;
 
         // row_container_main
+    }
+
+    public void selectDate(String type, String month, String dayOfMonth) {
+        if (!getSelectedMonth().equals(month)) {
+            if (type.equals("past")) {
+                swipeToRightUntilNeededMonth(month);
+
+                if (!getSelectedDayOfMonth().equals(dayOfMonth)) {
+                    swipeToRightUntilNeededDayOfMonth(dayOfMonth);
+                }
+
+            } else if (type.equals("future")) {
+                swipeToLeftUntilNeededMonth(month);
+                if (!getSelectedDayOfMonth().equals(dayOfMonth)) {
+                    swipeToLeftUntilNeededDayOfMonth(dayOfMonth);
+                }
+            }
+        } else if (!getSelectedDayOfMonth().equals(dayOfMonth)) {
+            if (type.equals("past")) {
+                swipeToRightUntilNeededDayOfMonth(dayOfMonth);
+            } else if (type.equals("future")) {
+                swipeToLeftUntilNeededDayOfMonth(dayOfMonth);
+            }
+        }
+    }
+
+    private void swipeToLeftUntilNeededDayOfMonth(String dayOfMonth) {
+        while (!getSelectedDayOfMonth().equals(dayOfMonth)) {
+            moveElementToLeft(By.id("info_viewPager"));
+            getSelectedDayOfMonth();
+        }
+    }
+
+    private void swipeToRightUntilNeededDayOfMonth(String dayOfMonth) {
+        while (!getSelectedDayOfMonth().equals(dayOfMonth)) {
+            moveElementToRight(By.id("info_viewPager"));
+            getSelectedDayOfMonth();
+        }
+    }
+
+    private void swipeToRightUntilNeededMonth(String month) {
+        while (!getSelectedMonth().equals(month)) {
+            moveElementToRight(By.id("info_viewPager"));
+            getSelectedMonth();
+        }
+    }
+
+    private void moveElementToRight(By locator) {
+        TouchAction action = new TouchAction(driver);
+        //get activity points
+        Dimension size = driver.manage().window().getSize();
+        int leftPoint = (int) (size.width * 0.2);
+        int rightPoint = (int) (size.width * 0.5);
+
+//get Element's point
+        WebElement element = driver.findElement(locator);
+
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        action
+                .longPress(PointOption.point(leftPoint, middleY))
+                .moveTo(PointOption.point(rightPoint, middleY))
+                .release()
+                .perform();
+    }
+
+    public void moveElementToLeft(By locator) {
+        TouchAction action = new TouchAction(driver);
+
+        Dimension size = driver.manage().window().getSize();
+        int leftPoint = (int) (size.width * 0.2);
+        int rightPoint = (int) (size.width * 0.5);
+
+        WebElement element = driver.findElement(locator);
+
+        int upperY = element.getLocation().getY();
+        int lowerY = upperY + element.getSize().getHeight();
+        int middleY = (upperY + lowerY) / 2;
+
+        action
+                .longPress(PointOption.point(rightPoint, middleY))
+                .moveTo(PointOption.point(leftPoint, middleY))
+                .release()
+                .perform();
+    }
+
+    private void swipeToLeftUntilNeededMonth(String month) {
+        while (!getSelectedMonth().equals(month)) {
+            moveElementToLeft(By.id("info_viewPager"));
+            getSelectedMonth();
+        }
+
+    }
+
+    private String getSelectedMonth() {
+        WebElement selectedDate = driver.findElement(By.id("date_container_layout"));
+        return selectedDate.findElement(By
+                        .xpath(".//*[@resource-id='com.example.svetlana.scheduler:id/row_month_txt']"))
+                .getText();
+    }
+
+    private String getSelectedDayOfMonth() {
+        WebElement selectedDate = driver.findElement(By.id("date_container_layout"));
+        return selectedDate.findElement(By.xpath(".//*[@resource-id='com.example.svetlana.scheduler:id/row_day_number_txt']"))
+                .getText();
     }
 
     //  +       fab_main
--- src/test/java/com/telran/scheduler/tests/EventCreationTests.java
+++ src/test/java/com/telran/scheduler/tests/EventCreationTests.java
@@ -38,4 +38,15 @@ public class EventCreationTests extends TestBase {
 
     }
 
+    @Test
+    public void eventCreationChangeDateTestOfRegisteredUserTest() throws InterruptedException {
+
+        app.getEvent().tapOnPlusButton();
+        app.getEvent().tapOnPencil();
+        app.getEvent().selectDate("future", "NOVEMBER", "30");
+
+        app.getEvent().fillEventCreationForm(new Event().setEventTitle("Event")
+                .setEventType("1").setBreaks(1).setWage("50"));
+        app.getEvent().tapOnAddEventButton();
+    }
 }
